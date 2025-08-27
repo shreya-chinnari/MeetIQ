d1bc20bf7da0084ebbd9cc031df3fd2352a50c4f
Shreya <shreya16180@gmail.com>
2025-08-27 13:20:39 +0530

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<>
+			<div className="pl-40 text-4xl font-bold text-green-600">
+				Hello world!
+			</div>
+
+			<Button>Click me</Button>
+		</>
+	);
+}
