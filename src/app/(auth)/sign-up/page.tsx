2e488a974d86d5242928d6dcbcaceba734f0d7af
Shreya <shreya16180@gmail.com>
2025-08-31 23:02:51 +0530

    authentication UI

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const page = () => {
+	console.log("sign up page");
+	return <SignUpView />;
+};
+
+export default page;
